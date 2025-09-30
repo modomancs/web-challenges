250407b930f0019bb226a846080d8b195cc89e97
Efstathios <modo575@hotmail.com>
2025-09-30 15:45:00 +0200

    finish challenge

---
+++ JS_basics/js-basics-and-variables_social-media-post/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a constant variable for each data point listed below:
+	- a title for the post
+	- text content for the post
+	- the number of likes the post has received
+	- the user who created the post
+	- a boolean called `isReported` to indicate whether the post has been reported
+*/
+
+// --v-- write your code here --v--
+const title = "Dogs";
+
+const text = "dogs are cool";
+
+const like = 32;
+
+const profile = "Alex";
+
+const isReported = false;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Then increase the number of likes by one and log the updated like count. Modify your code from step 1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(like);
+console.log(profile);
+console.log(isReported);
+const likesNow = like + 1;
+console.log(likesNow);
+// --^-- write your code here --^--
