d494b45eb489a61a01d6ae010e7481d3ffdf614b
Efstathios <modo575@hotmail.com>
2025-10-16 13:03:36 +0200

    finish

---
+++ js-structure/js-structure_quiz-app/components/App/App.js
@@ -0,0 +1,12 @@
+import { Header } from "../Header/header.js";
+import { Form } from "../Form/form.js";
+import { CardList } from "../Card/CardList.js";
+
+function App() {
+  const app = document.createElement("main");
+  app.classList.add("app");
+  app.append(Header(), Form(), CardList());
+
+  return app;
+}
+export { App };
---
+++ js-structure/js-structure_quiz-app/components/Card/CardList.js
@@ -0,0 +1,17 @@
+import { cards } from "../Card/cards.js";
+
+import { Card } from "../Card/Card.js";
+
+function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
+
+export { CardList };
