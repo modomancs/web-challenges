5a845578b89614af7605bfdf75427b392beb5b50
Efstathios <modo575@hotmail.com>
2025-10-23 16:32:53 +0200

    finish

---
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello broski</h1>
+      <p>wassup bro hows life</p>
+    </article>
+  );
+}
