7752ea6bbb8f796315b8319b7e4b4af16b3cb10f
Efstathios <modo575@hotmail.com>
2025-10-28 11:42:22 +0100

    finish

---
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+  function handleAdd() {
+    setPeople(people + 1);
+  }
+  function handleSubstraction() {
+    setPeople(people - 1);
+  }
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onAdd={handleAdd} onSubstraction={handleSubstraction} />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
---
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+export default function Counter({ onAdd, onSubstraction }) {
+  return (
+    <>
+      <h2>How many people would you like to visit us with?</h2>
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment people count"
+          onClick={onAdd}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement people count"
+          onClick={onSubstraction}
+        >
+          -
+        </button>
+      </div>
+    </>
+  );
+}
