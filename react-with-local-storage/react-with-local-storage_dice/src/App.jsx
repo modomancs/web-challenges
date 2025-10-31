f474ff67a1927c23b602f5d1ac0e6415891871b6
Efstathios <modo575@hotmail.com>
2025-10-31 11:38:37 +0100

    finish

---
+++ react-with-local-storage/react-with-local-storage_dice/src/App.jsx
@@ -0,0 +1,28 @@
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+import useLocalStorageState from "use-local-storage-state";
+import { getD6Roll } from "./utils";
+
+export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("dice-rolls", {
+    defaultValue: [],
+  });
+
+  function handleRoll() {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  }
+
+  const currentRollValue = rolls[0]?.value;
+
+  return (
+    <div className="app">
+      <main className="app__main">
+        <D6Button onRoll={handleRoll} value={currentRollValue} />
+      </main>
+      <aside className="app__aside">
+        <History rolls={rolls} />
+      </aside>
+    </div>
+  );
+}
